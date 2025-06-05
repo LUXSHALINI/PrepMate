c183773827fa9eaeb5db2e69c300b45a035d393f
LUXSHALINI <satheeskumarluxshalini@gmail.com>
2025-06-05 14:33:01 +0530

    my project

---
+++ controllers/authController.js
@@ -0,0 +1,40 @@
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '15d' });
+};
+
+export const signup = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: 'User already exists' });
+
+    const newUser = await User.create({ email, password });
+    const token = generateToken(newUser._id);
+
+    res.status(201).json({ userId: newUser._id, token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+
+    const token = generateToken(user._id);
+    res.status(200).json({ userId: user._id, token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
