e1de12a60e6b02d4e4cd0e1b1ba35b8a5225d35d
LUXSHALINI <satheeskumarluxshalini@gmail.com>
2025-07-02 14:30:51 +0530

    features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,12 @@ import Dashboard from './Pages/Dashboard';
 import StudentRegister from './Pages/StudentRegister';
 import PaymentForm from './components/PaymentForm';
 import Practice from './Pages/Practice';
-// import Features from './Pages/Features';
 import TrialConfirmation from './Pages/TrialConfirmation'; 
 import FreeTrialSignup from './Pages/FreeTrialSignup';
 import StudentList from './Pages/StudentList';
 import Register from './Pages/Register';
 import AdminDashboard from './Pages/AdminDashboard';
+
 
 
 
@@ -27,7 +27,6 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/studentregister" element={<StudentRegister/>} />
         <Route path="/payment" element={<PaymentForm />} />
-        {/* <Route path="/features" element={<Features />} /> */}
         <Route path="/trial-success" element={<TrialConfirmation />} />
         <Route path="/signup-trial" element={<FreeTrialSignup />} />
         <Route path="/practice" element={<Practice />} />
