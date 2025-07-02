ef5c2eea8a0ab4ad68075af60bffc2e65ce2c4cb
LUXSHALINI <satheeskumarluxshalini@gmail.com>
2025-07-02 12:30:59 +0530

    features

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
           </ul>
         </div>
 
-        {/* Social */}
+     
         <div>
           <h3 className="text-lg font-semibold mb-2">Follow Us</h3>
           <div className="flex gap-3">
